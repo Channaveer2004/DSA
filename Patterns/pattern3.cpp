d4ddd4ce71df84309a20950871eaed0ec62e7a12
Channaveer <channaveerbs0905@gmail.com>
2024-08-14 19:49:19 +0530

    ..

--- Patterns/pattern3.cpp
+++ Patterns/pattern3.cpp
@@ -23,63 +23,6 @@ int main()
         {
             cout << "*";
         }
-        cout << endl;// #include <iostream>
-// using namespace std;
-
-// int main() {
-//   int n;
-//   cout << "Enter n:";
-//   cin >> n;
-//   cout<<endl;
-//   cout<<endl;
-//   cout<<endl;
-
-//   for (int i = 0; i < n; i++) {
-//     for (int j = 0; j < i + 1; j++) {
-//       cout << "*";
-//     }
-  
-//     cout << endl;
-//   }
-//   for(int i =0; i<n;i++){
-//     for (int j = 0; j < n - i; j++) {
-//       cout << "*";
-//     }
-
-//     cout<<endl;
-//   }
-
-// }
-
-
-
-
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int n;
-    cout << "Enter N:";
-    cin >> n;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i + 1; j++)
-        {
-            cout << "*";
-        }
-
-        for (int j = 0; j < 2 * n - 2 * i - 2; j++)
-        {
-            cout << " ";
-        }
-
-        for (int j = 0; j < i + 1; j++)
-        {
-            cout << "*";
-        }
         cout << endl;
     }
 
@@ -96,7 +39,5 @@ int main()
         }
 
         cout<<endl;
-    }
-}
     }
 }
